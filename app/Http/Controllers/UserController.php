e687e4687fcef660517dfd2a9b7d995874db0072
huykhaduy <huykhaduy@gmail.com>
2023-12-17 21:28:53 +0700

    Update UserController.php
    revert data in user

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -41,7 +41,7 @@ class UserController extends Controller
 
     public function getHome()
     {
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $thong_bao = thongBao::where('id', '>', 0)->paginate(5, ['*'], 'noti');;
         $list_new = taiLieu::with('monHocChinh')->paginate(12, ['*'], 'news');
         return view('user.home', compact(['list_new', 'list_ctdt', 'thong_bao']));
@@ -49,7 +49,7 @@ class UserController extends Controller
 
     public function getThayDoiMatKhau()
     {
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $thong_bao = thongBao::where('id', '>', 0)->paginate(5, ['*'], 'noti');;
         $list_new = taiLieu::with('monHocChinh')->paginate(12, ['*'], 'news');
         return view('user.thay-doi-mat-khau', compact(['list_new', 'list_ctdt', 'thong_bao']));
@@ -58,7 +58,7 @@ class UserController extends Controller
     public function getThongBao()
     {
         $list_thong_bao = thongBao::orderBy('created_at', 'DESC')->paginate(10, ['*'], 'noti');
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $tai_lieu_moi = taiLieu::orderBy('created_at', 'DESC')->take(10)->get();
         $tai_lieu_lien_quan = taiLieu::inRandomOrder()->take(10)->get();
         return view('user.thong-bao', compact(['list_thong_bao', 'list_ctdt', 'tai_lieu_moi', 'tai_lieu_lien_quan']));
@@ -66,7 +66,7 @@ class UserController extends Controller
     public function getYeuCau()
     {
         $list_yeu_cau = YeuCau::orderBy('created_at', 'DESC')->where('user_id',Auth::id())->paginate(10, ['*'], 'noti');
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $tai_lieu_moi = taiLieu::orderBy('created_at', 'DESC')->take(10)->get();
         $tai_lieu_lien_quan = taiLieu::inRandomOrder()->take(10)->get();
         return view('user.yeu-cau', compact(['list_yeu_cau', 'list_ctdt', 'tai_lieu_moi', 'tai_lieu_lien_quan']));
@@ -78,7 +78,7 @@ class UserController extends Controller
         $list_tai_lieu = taiLieu::where('mon_hoc_chinh', $req['monHocId'])->paginate(6, ['*'], 'news');
         $mon_hoc = monHoc::where('id', $req['monHocId'])->first();
         $list_tai_lieu_khac = taiLieu::where('mon_hoc_chinh', '!=', $req['monHocId'])->get();
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         return view('user.tai-lieu-theo-mon', compact(['list_ctdt', 'list_tai_lieu', 'list_tai_lieu_khac', 'mon_hoc']));
     }
 
@@ -88,7 +88,7 @@ class UserController extends Controller
         $post = taiLieu::where('id', $req['postId'])->first();
         $post->luot_xem += 1;
         $post->save();
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $tai_lieu_moi = taiLieu::where('id', '!=', $post->id)->orderBy('created_at', 'DESC')->take(10)->get();
         $tai_lieu_lien_quan = taiLieu::where('mon_hoc_chinh', $post->mon_hoc_chinh)->where('id', '!=', $post->id)->paginate(12, ['*'], 'lienquan');
         return view('user.chi-tiet', compact(['list_ctdt', 'post', 'tai_lieu_lien_quan', 'tai_lieu_moi']));
@@ -98,7 +98,7 @@ class UserController extends Controller
     {
         $req = $request->all();
         $thong_bao = thongBao::where('id', $req['id'])->orderBy('created_at', 'DESC')->first();
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $tai_lieu_moi = taiLieu::orderBy('created_at', 'DESC')->take(10)->get();
         $thong_bao_khac = thongBao::where('id', '!=', $req['id'])->orderBy('created_at', 'DESC')->paginate(6, ['*'], 'notikhac');
         $tai_lieu_lien_quan = taiLieu::inRandomOrder()->take(10)->get();
@@ -108,7 +108,7 @@ class UserController extends Controller
     {
         $req = $request->all();
         $yeu_cau = YeuCau::where('id', $req['id'])->orderBy('created_at', 'DESC')->first();
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $tai_lieu_moi = taiLieu::orderBy('created_at', 'DESC')->take(10)->get();
         $yeu_cau_khac = YeuCau::where('id', '!=', $req['id'])->where('user_id',Auth::id())->orderBy('created_at', 'DESC')->paginate(6, ['*'], 'notikhac');
         $tai_lieu_lien_quan = taiLieu::inRandomOrder()->take(10)->get();
@@ -124,7 +124,7 @@ class UserController extends Controller
         }
         $tai_lieu_moi = taiLieu::where('id', '!=', 0)->orderBy('created_at', 'DESC')->take(10)->get();
         $tai_lieu_lien_quan = taiLieu::where('id', '!=', 0)->paginate(12, ['*'], 'lienquan');
-        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'DESC')->get();
+        $list_ctdt = chuongTrinhDaoTao::with('monHoc')->orderBy('created_at', 'ASC')->get();
         $list_tai_lieu = taiLieu::where('tag', 'like', '%' . $key . '%')->orWhere('ten_tai_lieu', 'like', '%' . $key . '%')->paginate(6, ['*'], 'news');
         return view('user.tim-kiem', compact(['list_ctdt', 'list_tai_lieu', 'tai_lieu_moi', 'tai_lieu_lien_quan']));
     }
