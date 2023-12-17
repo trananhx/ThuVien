5a8f45757c5934ce9486af5cbe97eae9a2da16b6
huykhaduy <huykhaduy@gmail.com>
2023-12-17 23:07:19 +0700

    thêm âm thanh thông  báo cho admin

--- public/administration/assets/js/kiem-tra-yeu-cau.js
+++ public/administration/assets/js/kiem-tra-yeu-cau.js
@@ -3,13 +3,22 @@ window.addEventListener("storage", () => {
     // console.log("Bell "+bell);
     if (!bell) return;
     const soLuongYeuCau = localStorage.getItem('thongBaoChuaXem') ?? 0;
+    const isRing = localStorage.getItem("amThanhThongBao") ?? "0";
     // console.log("So luong yeu cau: "+soLuongYeuCau);
     if (soLuongYeuCau > 0){
         bell.classList.add('show');
-        bell.setAttribute("title", "Bạn có "+soLuongYeuCau+ " phản hồi chưa xem!")
+        bell.setAttribute("title", "Bạn có " + soLuongYeuCau + " phản hồi chưa xem!");
+        if (isRing == 0) {
+            let audio = new Audio("../../files/anhBia/sound.mp3");
+            audio?.play();
+            localStorage.setItem("amThanhThongBao", "1");
+        }
     } else {
         bell.classList.remove('show');
-        bell.setAttribute("title", "")
+        bell.setAttribute("title", "Không có thông báo mới")
+        if (isRing == 1) {
+            localStorage.setItem("amThanhThongBao", "0");
+        }
     }
 });
 
