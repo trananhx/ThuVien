402dff13bd9db4828e840e7986ba9e1007b17a16
trananhx <84513904+trananhx@users.noreply.github.com>
2024-05-29 22:56:37 +0700

    Update search.blade.php

--- resources/views/user/layouts/search.blade.php
+++ resources/views/user/layouts/search.blade.php
@@ -6,7 +6,6 @@
                     style="background:url('https://png.pngtree.com/thumb_back/fw800/background/20230702/pngtree-educational-materials-3d-render-for-back-to-school-image_3728180.jpg')">
                     <div class="news-five-head aos" data-aos="fade-up">
                         <h2>BMTU Library</h2>
-                        <p style="color:blue"></p>
                     </div>
                     <div class="row justify-content-center">
                         <div class="col-lg-8 col-md-12">
