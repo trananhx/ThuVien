769d3c6814e5bdc3a107cf5c4c52adf7951898a2
trananhx <84513904+trananhx@users.noreply.github.com>
2024-05-31 22:17:53 +0700

    Update header.blade.php

--- resources/views/admin/layout/header.blade.php
+++ resources/views/admin/layout/header.blade.php
@@ -6,7 +6,7 @@
             <img src="https://bmtu.edu.vn/images/logo.png" style="width: 140px" alt="Logo">
         </a>
         <a href="{{route('user.home')}}" class="logo logo-small">
-            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="圖層_1"
+            <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="圖層_1"
                  x="0px" y="0px" width="40px" height="40px" viewBox="0 0 50 50" style="enable-background:new 0 0 50 50;"
                  xml:space="preserve">
                 <style type="text/css">
@@ -30,7 +30,7 @@
                               d="M33.6,31.7c-0.1-2.3,0-5,0-5c0-0.5,0-1.3,0.1-2.4c0.1-1.2,0.1-2.4,0.2-3.8l0.5-7.1c-1.5,0-2.7,0.1-3.5,0.1    c-0.7,0-1.9,0-3.7-0.1l-0.3,6.6c-0.1,1.4-0.1,2.4-0.2,3.1l-0.2,2.7c-0.1,1-0.1,1.8-0.2,2.3c0,0.5-0.1,1-0.1,1.2    c0,0.8,0.1,1.5,0.3,2.2c0.2,0.6,0.4,1.1,0.7,1.6c0.3,0.5,0.7,0.9,1.3,1.3c0.6,0.5,1.4,0.9,2.2,1.2c0.9,0.3,1.8,0.6,2.9,0.8    c1,0.2,2,0.2,3,0.3C35.3,36.1,33.7,34.1,33.6,31.7z"/>
                     </g>
                 </g>
-</svg>
+</svg> -->
         </a>
         <a id="toggle_btn">
             <i class="feather-chevrons-left"></i>
